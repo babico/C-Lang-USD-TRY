ce5490b3547bca40f24978462a198c6528811b10
Müslüm Barış Korkmazer <11800544+babico@users.noreply.github.com>
2022-03-06 02:10:34 +0300

    Update dolar-try-converter-online.c

--- dolar-try-converter-online.c
+++ dolar-try-converter-online.c
@@ -3,15 +3,18 @@
 #include <string.h>
 #include <curl/curl.h>
 
-struct string {
+struct string
+{
   char *ptr;
   size_t len;
 };
 
-void init_string(struct string *s) {
+void init_string(struct string *s)
+{
   s->len = 0;
-  s->ptr = malloc(s->len+1);
-  if (s->ptr == NULL) {
+  s->ptr = malloc(s->len + 1);
+  if (s->ptr == NULL)
+  {
     fprintf(stderr, "malloc() failed\n");
     exit(EXIT_FAILURE);
   }
@@ -20,17 +23,18 @@ void init_string(struct string *s) {
 
 size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
 {
-  size_t new_len = s->len + size*nmemb;
-  s->ptr = realloc(s->ptr, new_len+1);
-  if (s->ptr == NULL) {
+  size_t new_len = s->len + size * nmemb;
+  s->ptr = realloc(s->ptr, new_len + 1);
+  if (s->ptr == NULL)
+  {
     fprintf(stderr, "realloc() failed\n");
     exit(EXIT_FAILURE);
   }
-  memcpy(s->ptr+s->len, ptr, size*nmemb);
+  memcpy(s->ptr + s->len, ptr, size * nmemb);
   s->ptr[new_len] = '\0';
   s->len = new_len;
 
-  return size*nmemb;
+  return size * nmemb;
 }
 int main(void)
 {
@@ -41,7 +45,8 @@ int main(void)
   curl_global_init(CURL_GLOBAL_ALL);
 
   curl = curl_easy_init();
-  if(curl) {
+  if (curl)
+  {
     struct string s;
     init_string(&s);
 
@@ -49,20 +54,20 @@ int main(void)
     curl_easy_setopt(curl, CURLOPT_URL, "http://babico.tech/kur/trytousd.php");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-    
+
     res = curl_easy_perform(curl);
-    
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+
+    if (res != CURLE_OK)
+    {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    }
     curl_easy_cleanup(curl);
-    
+
     float para;
     printf("Kac dolariniz var: ");
     scanf("%f", &para);
     float sonuc = strtof(s.ptr, NULL) * para;
     printf("%.2f ABD Dolari %f kadar Turk Lirasina esit.\n", para, sonuc);
-    
   }
 
   curl_global_cleanup();
